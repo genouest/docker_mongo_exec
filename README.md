849bd929fac621dfad316a015687caac7ea8fc0b
mboudet <mateo.boudet@gmail.com>
2025-07-24 17:52:47 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mongo_exec
-Docker image to run a load data into a mongo database as a specific user.
+
+Custom Docker image to be used with [BEAURIS](https://gitlab.com/beaur1s/beauris)
+
+Will create a custom user, run mongo as that user, store the PID as an env variable (to stop it from the other process), and run a user command.
+
+If no user command is provided, it will simply launch mongo in the foreground.
+
+Available env variables:
+
+```
+TBA
+```
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function run_scripts () {
+	SCRIPTS_DIR="/scripts/$1.d"
+	SCRIPT_FILES_PATTERN="^${SCRIPTS_DIR}/[0-9][0-9][a-zA-Z0-9_-]+$"
+	SCRIPTS=$(find "$SCRIPTS_DIR" -type f -uid 0 -executable -regex "$SCRIPT_FILES_PATTERN" | sort)
+	if [ -n "$SCRIPTS" ] ; then
+		echo "=>> $1-scripts:"
+	    for script in $SCRIPTS ; do
+	        echo "=> $script"
+			. "$script"
+	    done
+	fi
+}
+
+
+
+run_scripts pre-launch
+
+# Default to launching mongo if no parameters passed
+if [ $# -eq 0 ]; then
+	su $MONGO_USER -c 'echo "bla"'
+else
+	su $MONGO_USER -c 'echo "bla" > /tmp/es.log 2>&1 &'
+	exec "$@"
+fi
